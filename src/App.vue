d098fb448631f98d716ac07a430a151dcbaac4e6
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-01-13 22:44:40 +0600

    💄 chore: install/setup tailwind css
    Signed-off-by: AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1 class="text-5xl font-bold underline">Hello world!</h1>
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
