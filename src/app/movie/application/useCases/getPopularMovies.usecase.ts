ce648ba534ff1f1cd8c58c924f07fa396798f88e
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-01-21 00:57:06 +0600

    refactor: update naming

--- src/app/movie/application/useCases/getPopularMovies.usecase.ts
+++ src/app/movie/application/useCases/getPopularMovies.usecase.ts
@@ -1,5 +1,5 @@
 import type { MovieInterface } from '@/app/movie/domain/movie.interface'
 
-export abstract class GetPopularMoviesUsecase {
+export abstract class GetPopularMoviesUseCase {
   abstract execute(page: number): Promise<MovieInterface[] | null>
 }
